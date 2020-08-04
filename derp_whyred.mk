796d0ef080f925e3ebb3eb964319bd8d6844a526
dinhsan2000 <dinhsan200@gmail.com>
2020-08-05 02:05:27 +0700

    Signed-off-by: dinhsan2000 <dinhsan200@gmail.com>
    Add XiaimiParts
    Enable FM Whyred
    Change Cherish_whyred.mk to derp_whyred.mk

--- derp_whyred.mk
+++ derp_whyred.mk
@@ -28,16 +28,11 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-# Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cherish.maintainer=Dinh San
-
-# Inherit some common Cherish stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common AOSIP STUFF.
+$(call inherit-product, vendor/aosip/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 
-PRODUCT_NAME := cherish_whyred
+PRODUCT_NAME := derp_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
